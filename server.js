4dde698bf7f6e931f8cdb6efacd2eeebf37a9b76
Sky <labidaniele@itis-molinari.eu>
2025-03-12 08:54:29 +0100

    prima modifica database

--- server.js
+++ server.js
@@ -1,31 +1,49 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const http = require("http");
+const mysql = require("mysql2");
+const path = require('path');
+
+app.use("/", express.static(path.join(__dirname, "public")));  //mettiamo a disposizione i dati nella cartella public
+
+//let todos = [];    non serve più questo dato
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: true
 }));
-const path = require('path');
-app.use("/", express.static(path.join(__dirname, "public")));
 
-let todos = [];
+//connessione al database
+const connessione = mysql.createConnection(
+   {
+      host: "mysql-371f75ea-itis-b1f2.b.aivencloud.com",
+      user: "avnadmin",
+      password: "AVNS_Q0tt-21uinsBZIath3z",
+      database: "defaultdb",
+      port: "24943",
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/ca.pem')
+      }
+    }
+);
 
 app.post("/todo/add", (req, res) => {
    const todo = req.body.todo;
    todo.id = "" + new Date().getTime();
-   todos.push(todo);
+   //todos.push(todo);
    res.json({result: "Ok"});
 });
 
 app.get("/todo", (req, res) => {
-   res.json({todos: todos});
+   //res.json({todos: todos});
 });
 
 app.put("/todo/complete", (req, res) => {
     const todo = req.body;
     try {
-       todos = todos.map((element) => {
+       todos = todos.map((element) => { //////////////////////
           if (element.id === todo.id) {
              element.completed = true;
           }
@@ -39,7 +57,7 @@ app.put("/todo/complete", (req, res) => {
 
 
  app.delete("/todo/:id", (req, res) => {
-    todos = todos.filter((element) => element.id !== req.params.id);
+    //todos = todos.filter((element) => element.id !== req.params.id);
     res.json({result: "Ok"});  
  })
 
